59b115d7aa1171ed5e16076715f917f18de404e8
landscapetec@163.com <tyl2011>
2020-11-09 22:29:38 +0800

    添加futuretask 和 completableFuture 实现

---
+++ Week_04/src/main/java/work_0702/CompletableFutureWork.java
@@ -0,0 +1,55 @@
+package work_0702;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
+ * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * 写出你的方法，越多越好，提交到github。
+ *
+ * 一个简单的代码参考：
+ */
+class CompletableFutureWork {
+
+    public static int result;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long start=System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+
+        CompletableFuture future=CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println(Thread.currentThread().getName());
+                result=sum();
+            }
+        });
+
+        while(!future.isDone()){
+            Thread.sleep(100);
+        }
+
+
+        //int result = sum(); //这是得到的返回值
+
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为："+result);
+
+        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // 然后退出main线程
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if ( a < 2)
+            return 1;
+        return fibo(a-1) + fibo(a-2);
+    }
+}
